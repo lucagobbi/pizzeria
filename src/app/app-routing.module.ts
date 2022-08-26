888378d8d7100ad2ea14ce02b7e91ea877631808
lucagobbi <lucasghegobbi@gmail.com>
2022-08-26 21:38:53 +0200

    fixed styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {DoveSiamoComponent} from './dove-siamo/dove-siamo.component';
 import {ContattaciComponent} from './contattaci/contattaci.component';
 import {PizzeComponent} from './pizze/pizze.component';
 
-const routes: Routes = [{path: 'pizze', component: PizzeComponent}, {path: 'dove-siamo', component: DoveSiamoComponent}, {path: 'contattaci', component: ContattaciComponent}];
+const routes: Routes = [{path: '', component: PizzeComponent}, {path: 'dove-siamo', component: DoveSiamoComponent}, {path: 'contattaci', component: ContattaciComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
